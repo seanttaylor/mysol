217441d3411c507a7c45e83e85077170e31d8355
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2019-09-07 17:39:52 -0400

    SCAFFOLDED mySol API SERVER.

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+function HTTPServer(bottle) {
+    /*Use of ES5 functions required with service declarations.
+     * See https://github.com/young-steveo/bottlejs/issues/103
+     */
+    const dependencies = ["middleware"];
+    bottle.service("HTTPServer", function(middleware) {
+        const http = require("http");
+        const express = require("express");
+        const app = express();
+        const bodyParser = require("body-parser");
+        const morgan = require("morgan");
+        const cors = require("cors");
+        const serverPort = 8080;
+
+        app.use(cors());
+        app.use(morgan("combined"));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
+
+        //app.use("/api/v1/posts", posts.controller);
+
+        http.createServer(app).listen(serverPort, () => {
+            console.log("mySōl server is listening on port %d (http://localhost:%d)", serverPort, serverPort);
+        });
+    }, ...dependencies);
+}
+
+module.exports = HTTPServer;
