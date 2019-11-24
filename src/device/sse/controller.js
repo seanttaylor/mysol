be4c7c37f20b6c3acd0068fd764eec25ff259b8c
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2019-11-24 16:14:29 +0000

    CREATED DEFAULT IMPLEMENTATION OF responseOf.

--- src/device/sse/controller.js
+++ src/device/sse/controller.js
@@ -1,14 +1,14 @@
 function sseController({ api, router }) {
 
     router.get("/", (req, res) => {
-        
+
         res.status(200).set({
             "connection": "keep-alive",
             "cache-control": "no-cache",
             "content-type": "text/event-stream"
         });
-        
-        api.setSSEResponseWriter((data)=> res.write(data));
+
+        api.setSSEResponseWriter((data) => res.write(data));
         res.write(api.eventOf("sse-channel"));
     });
 
--- src/device/sse/index.js
+++ src/device/sse/index.js
@@ -10,32 +10,47 @@ function sse(bottle) {
             eventOf
         };
 
+        let responseOf = defaultResponseOf;
+
         eventEmitter.on("device-battery-event", api.onDeviceBatteryEvent.bind(api));
         eventEmitter.on("device-telemetry-event", api.onDeviceTelemetryEvent.bind(api));
 
         /**
-         * Creates a UUID.
-         * @returns {String} 
-        */
+         * Default implementation of responseOf.
+         * Logs Server-Sent events to the console if no clients to connect to
+         * the server.
+         * @param {Object} data - Event data to push to connected clients.
+         * @returns void
+         */
 
-        function createUUID(){
+        function defaultResponseOf(data) {
+            console.log("/*===New Event===*/");
+            console.log(data);
+        }
+
+        /**
+         * Creates a UUID.
+         * @returns {String}
+         */
+
+        function createUUID() {
             let dt = new Date().getTime();
             let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                let r = (dt + Math.random()*16)%16 | 0;
-                dt = Math.floor(dt/16);
-                return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+                let r = (dt + Math.random() * 16) % 16 | 0;
+                dt = Math.floor(dt / 16);
+                return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
             });
             return uuid;
         }
-        
+
         /**
          * Creates an event data object for subscribers on the server-sent event stream to consume.
          * @param {String} eventName - The name of the event to create.
          * @param {Object} payload - Data associated with event.
-         * @returns {String} 
-        */
+         * @returns {String}
+         */
 
-        function eventOf(eventName, payload={}) {
+        function eventOf(eventName, payload = {}) {
             return `data: ${JSON.stringify({
                 header: {
                     timestamp: new Date().toISOString(),
@@ -46,34 +61,34 @@ function sse(bottle) {
             })}\n\n`;
         }
 
-         /**
-         * Adds an Express response object to the API module so methods on the API can publish 
+        /**
+         * Adds an Express response object to the API module so methods on the API can publish
          * data to the event stream.
          * @param {Function} responseWriterFn - A function wrapper of the Express response object.
          * @returns void
-        */
+         */
 
         function setSSEResponseWriter(responseWriterFn) {
-            this.responseOf = responseWriterFn;
+            responseOf = responseWriterFn;
         }
 
         /**
          * Publishes a server-sent event to the client with battery data.
          * @param {Object} batteryData - Current data about the battery.
          * @returns void
-        */
+         */
 
         function onDeviceBatteryEvent(batteryData) {
-            this.responseOf(eventOf("device-battery-event", batteryData));
+            responseOf(eventOf("device-battery-event", batteryData));
         }
 
         /**
          * Publishes a server-sent event to the client with telemetry data.
          * @param {Object} telemetryData - Current telemetry data.
-        */
+         */
 
         function onDeviceTelemetryEvent(telemetryData) {
-            this.responseOf(eventOf("device-telemetry-event", telemetryData));
+            responseOf(eventOf("device-telemetry-event", telemetryData));
         }
 
 
